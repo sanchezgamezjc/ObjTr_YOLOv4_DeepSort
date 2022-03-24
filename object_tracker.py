d000b6d93f61ce92c29b86b64859ed393e21543d
Jose Sanchez <josesanchez@air-de-jose.home>
2022-03-24 15:56:47 +0100

    Act 24.03.2022

--- object_tracker.py
+++ object_tracker.py
@@ -219,7 +219,7 @@ def main(_argv):
             cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
 
             
-            tracking_frame[track.track_id] = {'x':(int(bbox[0])+((int(bbox[2])-int(bbox[0]))/2), 'y': int(bbox[1])} 
+            tracking_frame[track.track_id] = {'x':(int(bbox[0]))+((int(bbox[2])-int(bbox[0]))/2), 'y': int(bbox[1])} 
 
         # if enable info flag then print details about each track
             if FLAGS.info:
@@ -242,7 +242,8 @@ def main(_argv):
         if cv2.waitKey(1) & 0xFF == ord('q'): break
     cv2.destroyAllWindows()
 
-    print(tracking_data)
+    with open("my.json","w") as f:
+        json.dump(tracking_data,f)
 
 if __name__ == '__main__':
     try:
